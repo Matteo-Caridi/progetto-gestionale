e0a1021a20112df77770376bd4986270874dc667
Matteo-Caridi <caridimatteo0@gmail.com>
2023-10-19 11:03:28 +0200

    nuovo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,31 @@
 import { UserPageComponent } from '../app/user-page/user-page.component';
 import { CanDeactivateGuard } from '../guard/candeactive-guard';
-import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from "@angular/core";
-
+import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'userRegister', component: UserRegisterComponent, canDeactivate: [CanDeactivateGuard] },
-    { path: 'userRegister/:id', component: UserRegisterComponent, canDeactivate: [CanDeactivateGuard] },
-    { path: 'userPage/:id', component: UserPageComponent },
-    { path: 'userList', component: UsersListComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', redirectTo: '/home', pathMatch: 'full' }
-]
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'userRegister',
+    component: UserRegisterComponent,
+    canDeactivate: [CanDeactivateGuard],
+  },
+  {
+    path: 'userRegister/:id',
+    component: UserRegisterComponent,
+    canDeactivate: [CanDeactivateGuard],
+  },
+  { path: 'userPage/:id', component: UserPageComponent },
+  { path: 'userList', component: UsersListComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
